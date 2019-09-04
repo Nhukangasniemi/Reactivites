9eb9ac76c77c2bbce982a6dbe1d982a9bcc6123b
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2019-09-04 12:59:56 +0300

    End of section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,37 +1,36 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { cars } from './demo';
-import CarItem from './CarItem';
-import axios from 'axios';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
 
 class App extends Component {
-
   state = {
-    values: [],
-  }
+    values: []
+  };
 
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values')
-      .then(res => {
-        this.setState({
-          values: res.data
-        })
-      })
+    axios.get("http://localhost:5000/api/values").then(res => {
+      this.setState({
+        values: res.data
+      });
+    });
   }
 
   render() {
     return (
-      <div className="App">
-        <ul>
-          {
-            this.state.values.map((val: any) => (
-              <li key={val.id}>{val.name}</li>
-            ))
-          }
-        </ul>
+      <div>
+        <Header as="h2">
+          <Icon name="users" />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((val: any) => (
+            <List.Item key={val.id}>{val.name}</List.Item>
+          ))}
+        </List>
       </div>
-    )
+    );
   }
 }
 
