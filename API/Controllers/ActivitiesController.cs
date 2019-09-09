e95dc3eac9dacb4a702ade63d3c0a4debfc22ee9
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2019-09-09 12:57:50 +0300

    Edit handler Added with Code snippets

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -37,5 +37,12 @@ namespace API.Controllers
         {
             return await _mediator.Send(command);
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
     }
 }
