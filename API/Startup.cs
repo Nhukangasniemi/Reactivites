c4c774dee5ed6c60602f2c910b62ccf56fdfbe6d
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2019-10-21 23:15:13 +0300

    Create ErrorHandlingMiddleware

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Middleware;
 using Application.Activities;
 using FluentValidation.AspNetCore;
 using MediatR;
@@ -46,6 +47,7 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
             if (env.IsDevelopment())
             {
                 // app.UseDeveloperExceptionPage();
--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using FluentValidation;
 using MediatR;
 using Persistence;
@@ -45,8 +47,8 @@ namespace Application.Activities
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Id);
-                if (activity == null)
-                    throw new Exception("Could not find activity");
+                 if(activity == null)
+                    throw new RestException(HttpStatusCode.NotFound, new {activity = "Not found"});
                 activity.Title = request.Title ?? activity.Title;
                 activity.Description = request.Description ?? activity.Description;
                 activity.Category = request.Category ?? activity.Category;
