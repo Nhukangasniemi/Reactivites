3f79d36d90ad93b1e60865bfc604f8c885b81f8b
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2020-01-08 13:47:24 +0200

    Getting currently logged in user

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -6,19 +6,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
-    public class UserController: BaseController
+
+    public class UserController : BaseController
     {
+        [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<User>> Login(Login.Query query)
         {
             return await Mediator.Send(query);
         }
 
+        [AllowAnonymous]
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register(Register.Command command)
         {
             return await Mediator.Send(command);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User>> CurrentUser()
+        {
+            return await Mediator.Send(new CurrentUser.Query());
         }
     }
 }
---
+++ Application/User/CurrentUser.cs
@@ -0,0 +1,43 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Persistence;
+
+namespace Application.User
+{
+    public class CurrentUser
+    {
+        public class Query : IRequest<User> { }
+
+        public class Handler : IRequestHandler<Query, User>
+        {
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IJwtGenerator _jwtGenerator;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(UserManager<AppUser> userManager, IJwtGenerator jwtGenerator, IUserAccessor userAccessor)
+            {
+
+                _userManager = userManager;
+                _jwtGenerator = jwtGenerator;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
+            {
+                //handler logic goes here
+                var user = await _userManager.FindByNameAsync(_userAccessor.GetCurrentUsername());
+                return new User 
+                {
+                    DisplayName = user.DisplayName,
+                    Username = user.UserName,
+                    Token = _jwtGenerator.CreateToken(user),
+                    Image = null
+                };
+            }
+        }
+    }
+}
