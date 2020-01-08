5a35ed0487cf714ef67f38b37d6c373276724b42
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2020-01-08 09:51:38 +0200

    Generate Jwt Token

--- Infrastructure/Security/JwtGenerator.cs
+++ Infrastructure/Security/JwtGenerator.cs
@@ -1,13 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Application.Interfaces;
 using Domain;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure.Security
 {
     public class JwtGenerator : IJwtGenerator
     {
-     public string CreateToken(AppUser user)
-     {
-         throw new System.NotImplementedException();
-     }   
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+         {
+             new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+         };
+            //generatw signing credentials
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
