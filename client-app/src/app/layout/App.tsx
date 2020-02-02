d428c5b20debb8d854f122d4e40a724bfd724e2d
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2020-02-02 16:28:01 +0200

    Create Login Form and Routing to Login Page

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -16,6 +16,7 @@ import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import NotFound from "./NotFound";
 import {ToastContainer} from 'react-toastify'
+import LoginForm from './../../features/user/LoginForm';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
   return (
@@ -36,6 +37,7 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
                   path={["/createActivity", "/manage/:id"]}
                   component={ActivityForm}
                 />
+                <Route path="/login" component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
---
+++ client-app/src/features/user/LoginForm.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Form as FinalForm, Field } from "react-final-form";
+import { Form, Button } from "semantic-ui-react";
+import { TextInput } from "./../../app/common/form/TextInput";
+
+const LoginForm = () => {
+  return (
+    <FinalForm
+      onSubmit={values => console.log(values)}
+      render={({ handleSubmit }) => (
+        <Form onSubmit={handleSubmit}>
+          <Field name="email" component={TextInput} placeholder="Email" />
+          <Field
+            name="password"
+            component={TextInput}
+            placeholder="Password"
+            type="Password"
+          />
+          <Button positive content="Login" />
+        </Form>
+      )}
+    />
+  );
+};
+
+export default LoginForm;
