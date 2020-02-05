5e1ae8837b1efb2b4c50cf8a3dd5871cb550af74
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2020-02-05 12:41:23 +0200

    Join table UserActivity and Add to migrations

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -9,9 +9,10 @@ namespace Persistence
         public DataContext(DbContextOptions options) : base(options)
         {
         }
-        
+
         public DbSet<Value> Values { get; set; }
         public DbSet<Activity> Activities { get; set; }
+        public DbSet<UserActivity> UserActivities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -19,10 +20,16 @@ namespace Persistence
 
             builder.Entity<Value>()
                 .HasData(
-                    new Value {Id = 1, Name = "Value 101"},
-                    new Value {Id = 2, Name = "Value 102"},
-                    new Value {Id = 3, Name = "Value 103"}
+                    new Value { Id = 1, Name = "Value 101" },
+                    new Value { Id = 2, Name = "Value 102" },
+                    new Value { Id = 3, Name = "Value 103" }
                 );
+            builder.Entity<UserActivity>(x => x.HasKey(ua => new { ua.AppUserId, ua.ActivityId }));
+
+            builder.Entity<UserActivity>().HasOne(u => u.AppUser)
+            .WithMany(a => a.UserActivities).HasForeignKey(u => u.AppUserId);
+            builder.Entity<UserActivity>().HasOne(u => u.Activity)
+            .WithMany(a => a.UserActivities).HasForeignKey(u => u.ActivityId);
         }
     }
 }
