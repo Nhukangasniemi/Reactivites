c8b6ee1d2006ba3acedd2feb55eb73634cda4693
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2020-02-20 19:20:43 +0200

    Add Photo Command Handler

--- Infrastructure/Photos/PhotoAccessor.cs
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -1,3 +1,4 @@
+using System;
 using Application.Interfaces;
 using Application.Photos;
 using CloudinaryDotNet;
@@ -34,6 +35,8 @@ namespace Infrastructure.Photos
                     uploadResult = _cloudinary.Upload(uploadParams);
                 }
             }
+            if(uploadResult.Error != null)
+                throw new Exception(uploadResult.Error.Message);
             return new PhotoUploadResult
             {
                 PublicId = uploadResult.PublicId,
