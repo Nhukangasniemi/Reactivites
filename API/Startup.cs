208549bc1281012d4c1627747c3f61bf1f509e76
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2020-02-24 09:05:59 +0200

    Create SignalR Hub

--- API/Startup.cs
+++ API/Startup.cs
@@ -57,6 +57,7 @@ namespace API
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddAutoMapper(typeof(List.Handler));
+            services.AddSignalR();
             services.AddControllers(opt =>
             {
                 var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
@@ -115,6 +116,7 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chat");
             });
         }
     }
